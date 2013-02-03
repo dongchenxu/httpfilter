ee3d9b6ee108af0359e146f5e9d188227460876f
vlinux <oldmanpushcart@gmail.com>
2013-02-03 22:16:38 +0800

    replae

--- httpfilter-proxy/src/main/java/com/googlecode/httpfilter/proxy/org/khelekore/rnio/package-info.java
+++
@@ -1,8 +0,0 @@
-/** The basic interfaces for rnio.
- *
- *  <p>The main class here is the {@link org.khelekore.rnio.NioHandler}.
- *
- * @since 1.0
- */
-package com.googlecode.httpfilter.proxy.org.khelekore.rnio;
-
