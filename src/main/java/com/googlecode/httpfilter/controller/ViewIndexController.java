d307519ab05437f1987bbcd0f4096a8df20a1fef
dongchenxu <luanjia.hz@gmail.com>
2013-02-13 03:14:36 +0800

    修改类名

--- src/main/java/com/googlecode/httpfilter/controller/ViewIndexController.java
+++ src/main/java/com/googlecode/httpfilter/controller/ViewIndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class viewIndexController {
+public class ViewIndexController {
 
 	@RequestMapping("/index.do")
 	public String viewIndex(){
